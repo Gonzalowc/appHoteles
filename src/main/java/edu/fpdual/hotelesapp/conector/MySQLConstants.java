3f50389ba93a1f1cce53667ddeb591583862d3ba
Ángela Bonilla <angelaagmez@gmail.com>
2021-05-07 09:47:22 +0200

    Add database connection

---
+++ src/main/java/edu/fpdual/hotelesapp/conector/MySQLConstants.java
@@ -0,0 +1,18 @@
+package edu.fpdual.hotelesapp.conector;
+
+/**
+ * Clase MySQL Constants.
+ *
+ */
+public class MySQLConstants {
+
+	public static final String PASSWD = "jdbc.mysql.passwd";
+	public static final String USER = "jdbc.mysql.user";
+	public static final String DRIVER = "jdbc.mysql.driver";
+	public static final String URL_PREFIX = "jdbc.mysql.url.prefix";
+	public static final String URL_HOST = "jdbc.mysql.url.host";
+	public static final String URL_PORT = "jdbc.mysql.url.port";
+	public static final String URL_SCHEMA = "jdbc.mysql.url.schema";
+	public static final String URL_SSL = "jdbc.mysql.url.ssl";
+	
+}
