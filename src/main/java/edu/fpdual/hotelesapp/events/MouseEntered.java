de6a4e466b4ffd57535632181f2b8989b32cdaea
Gonzalo Waack Carneado <44806755+Gonzalowc@users.noreply.github.com>
2021-05-12 12:45:56 +0200

    acceso Login

---
+++ src/main/java/edu/fpdual/hotelesapp/events/MouseEntered.java
@@ -0,0 +1,11 @@
+package edu.fpdual.hotelesapp.events;
+
+public class MouseEntered {
+	
+	
+	
+	public void cerrarPrograma() {
+		System.exit(0);
+	}
+
+}
---
+++ src/main/java/edu/fpdual/hotelesapp/hotelesapp/LoginController.java
@@ -0,0 +1,47 @@
+package edu.fpdual.hotelesapp.hotelesapp;
+
+import java.io.IOException;
+
+import edu.fpdual.hotelesapp.conector.Conector;
+import edu.fpdual.hotelesapp.manejadordb.ManejadorUsuario;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+	@FXML
+	private TextField txtUsuario;
+	@FXML
+	private PasswordField txtPassword;
+	@FXML
+	private Label msgError;
+	
+	
+	public void exit() {
+		System.exit(0);
+	}
+	
+	@FXML
+    private void switchToSecondary() throws IOException {
+		
+        App.setRoot("secondary");
+    }
+	
+	
+	public void validar() throws IOException {
+		Conector con = new Conector();
+		ManejadorUsuario mu = new ManejadorUsuario();
+		if(mu.validarUsuario(con, txtUsuario.getText(), txtPassword.getText())) {
+			msgError.setVisible(false);
+			txtUsuario.setText("");
+			txtPassword.setText("");
+			App.setRoot("menu");
+		}else {
+			msgError.setVisible(true);
+			txtPassword.setText("");
+			
+		}
+	}
+
+}
