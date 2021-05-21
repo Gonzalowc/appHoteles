b7dfbbd0a55d42a706f9788f434f97e457591a15
Gonzalo Waack Carneado <44806755+Gonzalowc@users.noreply.github.com>
2021-05-21 18:47:08 +0200

    init test

---
+++ src/test/java/edu/fpdual/hotelesapp/conector/ConectorTest.java
@@ -0,0 +1,14 @@
+package edu.fpdual.hotelesapp.conector;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConectorTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
